ca72f85f5799338a9fc66b16a8af395835ce5f16
httpakrx <bhnsspmng@gmail.com>
2024-08-17 20:29:52 +0800

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = [always open po kami","available nitro, robux, etc.","bili ka na mwuaahh~"];
 
 
 let currentIndex = 0;
